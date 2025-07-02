9a0c4b2595b0929e9d0cbcaae24f81f7f3ad18a4
shivenaggarwal <shivenagg04@gmail.com>
2025-07-02 21:46:03 +0530

    agent kit test

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer. You write readbable, maintainable code. You write simple next.js and react snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `write the following snippet: ${event.data.value}`
+    );
+    return { output };
   }
 );
