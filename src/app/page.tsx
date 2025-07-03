58c4571497465a4a2d32da814e668fe1d9f9e986
shivenaggarwal <shivenagg04@gmail.com>
2025-07-04 00:11:24 +0530

    ui update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
+  const router = useRouter();
+
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
@@ -15,6 +18,9 @@ const Page = () => {
     trpc.projects.create.mutationOptions({
       onError: (error) => {
         toast.error(error.message);
+      },
+      onSuccess: (data) => {
+        router.push(`/projects/${data.id}`);
       },
     })
   );
