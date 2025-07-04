2ba8772be988ebe96f15a7d79b5fb909f2f54983
shivenaggarwal <shivenagg04@gmail.com>
2025-07-04 18:11:53 +0530

    message form

--- src/modules/projects/ui/components/message-container.tsx
+++ src/modules/projects/ui/components/message-container.tsx
@@ -1,19 +1,35 @@
 import { MessageCard } from "@/modules/projects/ui/components/message-card";
+import { MessageForm } from "@/modules/projects/ui/components/message-form";
 import { useTRPC } from "@/trpc/client";
-import { trpc } from "@/trpc/server";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
 
 interface Props {
   projectId: string;
 }
 
 export const MessagesContainer = ({ projectId }: Props) => {
+  const bottomRef = useRef<HTMLDivElement>(null);
   const trpc = useTRPC();
   const { data: messages } = useSuspenseQuery(
     trpc.messages.getMany.queryOptions({
       projectId: projectId,
     })
   );
+
+  useEffect(() => {
+    const lastAssistantMessage = messages.findLast(
+      (message) => message.role === "ASSISTANT"
+    );
+
+    if (lastAssistantMessage) {
+      // todo set active fragment
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView();
+  }, [messages.length]);
 
   return (
     <div className="flex flex-col flex-1 min-h-0">
@@ -31,7 +47,12 @@ export const MessagesContainer = ({ projectId }: Props) => {
               type={message.type}
             />
           ))}
+          <div ref={bottomRef} />
         </div>
+      </div>
+      <div className="relative p-3 pt-1">
+        <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background/70 pointer-events-none" />
+        <MessageForm projectId={projectId} />
       </div>
     </div>
   );
---
+++ src/modules/projects/ui/components/message-form.tsx
@@ -0,0 +1,122 @@
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+interface Props {
+  projectId: string;
+}
+
+const formSchema = z.object({
+  value: z
+    .string()
+    .min(1, { message: "value is required" })
+    .max(10000, { message: "value is too long" }),
+});
+
+export const MessageForm = ({ projectId }: Props) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      value: "",
+    },
+  });
+
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        form.reset();
+        queryClient.invalidateQueries(
+          trpc.messages.getMany.queryOptions({
+            projectId,
+          })
+        );
+        // todo invalidate usage stats
+      },
+      onError: (error) => {
+        // todo redired to pricing page if out of credits
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    await createMessage.mutateAsync({
+      value: values.value,
+      projectId,
+    });
+  };
+
+  const [isFocused, setIsFocused] = useState(false);
+  const showUsage = false;
+  const isPending = createMessage.isPending;
+  const isBtnDisabled = isPending || !form.formState.isValid;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn(
+          "relative border p-4 pt-1 rounded-xl bg-sidebar dark:bg-sidebar transition-all",
+          isFocused && "shodow-xs",
+          showUsage && "rounded-t-none"
+        )}
+      >
+        <FormField
+          control={form.control}
+          name="value"
+          render={({ field }) => (
+            <TextareaAutosize
+              disabled={isPending}
+              {...field}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              minRows={2}
+              maxRows={8}
+              className="pt-4 resize-none border-none w-full outline-none bg-transparent"
+              placeholder="ask lunaris to build..."
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+                  e.preventDefault();
+                  form.handleSubmit(onSubmit)(e);
+                }
+              }}
+            />
+          )}
+        />
+        <div className="flex gap-x-2 items-end justify-between pt-2">
+          <div className="text-[10px] text-muted-foreground font-mono">
+            <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+              <span>&#8984;</span>Enter
+            </kbd>
+            &nbsp;to submit
+          </div>
+          <Button
+            disabled={isBtnDisabled}
+            className={cn(
+              "szie-8 rounded-full",
+              isBtnDisabled && "bg-muted-foreground border"
+            )}
+          >
+            {isPending ? (
+              <Loader2Icon className="size-4 animate-spin" />
+            ) : (
+              <ArrowUpIcon />
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
